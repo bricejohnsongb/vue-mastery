3eb984b09f2c4883038f17ddf9c9b5f46cbee8bf
Brice Johnson <brice.johnson@grainbridge.com>
2019-11-06 16:10:04 -0600

    animating-vue: 3/5

--- animating-vue/src/views/Modal.vue
+++ animating-vue/src/views/Modal.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="home">
+  <div>
+    <h1>This is the modal page</h1>
+
     <button @click="toggleModal">Open</button>
+
     <transition name="fade">
       <div v-if="isOpen" class="modal">
-        <button @click="toggleModal">Close</button>
+        <p>
+          <button @click="toggleModal">Close</button>
+        </p>
       </div>
     </transition>
   </div>
@@ -25,16 +30,4 @@ export default {
 </script>
 
 <style>
-.fade-enter {
-  opacity: 0;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease-out;
-}
-
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
