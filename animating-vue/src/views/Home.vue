cdb1b9e01b997ca3e60c394a3324e749b636d287
Brice Johnson <brice.johnson@grainbridge.com>
2019-11-06 15:32:08 -0600

    animatingVue: 2/5

--- animating-vue/src/views/Home.vue
+++ animating-vue/src/views/Home.vue
@@ -1,25 +1,40 @@
 <template>
   <div class="home">
     <button @click="toggleModal">Open</button>
-    <div v-if="isOpen" class="modal">
-      <button @click="toggleModal">Close</button>
-    </div>
+    <transition name="fade">
+      <div v-if="isOpen" class="modal">
+        <button @click="toggleModal">Close</button>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       isOpen: false
     }
   },
   methods: {
-    toggleModal() {
+    toggleModal () {
       this.isOpen = !this.isOpen
     }
   }
 }
 </script>
 
-<style></style>
+<style>
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
